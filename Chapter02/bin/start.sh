24130471c3d583049df680c9449798e347fdceb5
Eric Rodriguez <eric.rodriguez@eniro.com>
2019-08-24 03:44:59 +0200

    Update Sparkling Water for Sparl 2.4

--- Chapter02/bin/start.sh
+++ Chapter02/bin/start.sh
@@ -3,12 +3,10 @@ set -e
 TOPDIR="$(cd `dirname $0`/..; pwd)"
 export DATADIR="$TOPDIR/data"
 
-export SPARKLING_WATER_VERSION="2.1.12"
+export SPARKLING_WATER_VERSION="3.26.2-2.4"
 export SPARK_PACKAGES=\
-"ai.h2o:sparkling-water-core_2.11:${SPARKLING_WATER_VERSION},\
-ai.h2o:sparkling-water-repl_2.11:${SPARKLING_WATER_VERSION},\
-ai.h2o:sparkling-water-ml_2.11:${SPARKLING_WATER_VERSION},\
-com.packtpub:mastering-ml-w-spark-utils:1.0.0"
+"ai.h2o:sparkling-water-package_2.11:${SPARKLING_WATER_VERSION},\
+com.packtpub:mastering-ml-w-spark-utils_2.11:1.0.0-SNAPSHOT"
 
 $SPARK_HOME/bin/spark-shell \
         --master 'local[*]' \
